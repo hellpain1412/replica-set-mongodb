8ea6e70b42e43b4ec7e50d1f81953d8a0cd438de
T E N <tienphamhd.dev@gmail.com>
2025-11-05 18:46:16 +0700

    feat: Update DNS and IP configurations in certificate generation for MongoDB setup

--- setup.sh
+++ setup.sh
@@ -44,8 +44,9 @@ subjectAltName = @alt_names
 
 [ alt_names ]
 DNS.1 = ${host}
-DNS.2 = ${host}.mongo-net
+DNS.2 = mongo.ataji.ataji.group
 IP.1  = 127.0.0.1
+IP.2  = 103.166.185.163
 EOF
     
     openssl genrsa -out certs/${host}.key 2048
